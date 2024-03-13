992d5cbf8b15f1e022aa08c431938ae40ade40bf
MarioBroi <inoix16@gmail.com>
2024-03-13 09:35:07 +0000

    add bonus

--- bonus/assets/js/app.js
+++ bonus/assets/js/app.js
@@ -3,33 +3,63 @@ console.log('test');
 const { createApp } = Vue;
 
 createApp({
-    setup() {
+    data() {
         return {
             activeImage: 0,
             slides: [
                 {
-                    image: "./assets/img/01.webp",
+                    image: "img/01.webp",
                     title: 'Marvel\'s Spiderman Miles Morale',
                     text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
                 }, {
-                    image: "./assets/img/02.webp",
+                    image: "img/02.webp",
                     title: 'Ratchet & Clank: Rift Apart',
                     text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
                 }, {
-                    image: "./assets/img/03.webp",
+                    image: "img/03.webp",
                     title: 'Fortnite',
                     text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
                 }, {
-                    image: "./assets/img/04.webp",
+                    image: "img/04.webp",
                     title: 'Stray',
                     text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
                 }, {
-                    image: "./assets/img/05.webp",
+                    image: "img/05.webp",
                     title: "Marvel's Avengers",
                     text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
                 }
             ]
 
         }
+    },
+    methods: {
+        prev() {
+            console.log('prev');
+            this.activeImage--
+            if (this.activeImage < 0) {
+                whis.activeImage = this.slides.image.length - 1;
+            }
+        },
+        next() {
+            console.log('next');
+            this.activeImage++
+            if (this.activeImage === this.slides.length) {
+                this.activeImage = 0;
+            }
+        },
+        startAutoplay() {
+            this.intervalId = setInterval(() => {
+                console.log('this');
+                this.next();
+            }, 3000);
+        },
+        stopAutoplay() {
+            clearInterval(this.intervalId);
+        }
+    },
+    mounted() {
+        this.loopSlide();
+
     }
+
 }).mount('#app')
